0b6de56f2f8a5b65a0b1956920ab09966cdc7af9
Bruno Passuello <bruno.fogazzi.passuello@gmail.com>
2024-09-27 10:50:07 -0300

    Main

---
+++ Main.py
@@ -0,0 +1,6 @@
+from Jogador import Jogador
+from Clube import Clube
+
+jogador = Jogador("Neymar", 29, "Atacante", 1.75, 68.5, False)
+clube = Clube("Paris Saint-Germain")
+clube.contratar_jogador(jogador)
