3f7b476d49d25beb24197e7574e0835ba210a030
Henrique Rafael Sousa de Oliveira <henrique.sousaufsc@gmail.com>
2024-11-03 20:26:30 -0300

    verificação tela_sistema e ajustes controlador_clube

--- telas/tela_sistema.py
+++ telas/tela_sistema.py
@@ -1,17 +1,24 @@
 class TelaSistema:
     def tela_opcoes(self):
-        mensagem = '''
-        ----------BID_2----------
+        while True:
+            mensagem = '''
+            ----------BID_2----------
 
-        Escolha uma Opção:
+            Escolha uma Opção:
 
-        * Clube - 1
-        * Jogador - 2
-        * Técnico - 3
-        * Campeonato - 4
-        * Contrato - 5
-        * Finalizar Programa - 0
-        '''
-        print(mensagem)
-        comando = int(input("Escolha sua opção: "))
-        return comando
+            * Clube - 1
+            * Jogador - 2
+            * Técnico - 3
+            * Campeonato - 4
+            * Contrato - 5
+            * Finalizar Programa - 0
+            '''
+            print(mensagem)
+            try:
+                comando = int(input("Escolha sua opção: "))
+                if comando in range(6):  # 0 a 5
+                    return comando
+                else:
+                    print("Opção inválida! Por favor, escolha um número entre 0 e 5.")
+            except ValueError:
+                print("Entrada inválida! Por favor, digite um número.")
